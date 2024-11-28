d97711c5fc7f177a184290142917e992686273a2
retornam <retornam@users.noreply.github.com>
2024-11-28 12:09:08 -0800

    allow full rebuild
    Signed-off-by: Raymond Etornam <retornam@users.noreply.github.com>

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 #hugo server --buildDrafts --buildFuture --watch
-hugo server --bind 0.0.0.0 --port 8080 --baseURL http://$(ifconfig en0 | grep "inet " | awk '{ print $2 }'):8080 --buildDrafts --buildFuture --watch
+hugo server --bind 0.0.0.0 --port 8080 --baseURL http://$(ifconfig en0 | grep "inet " | awk '{ print $2 }'):8080 --buildDrafts --buildFuture --disableFastRender --watch
